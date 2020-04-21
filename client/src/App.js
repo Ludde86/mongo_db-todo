8b2c9ab92cf86ade646786f7afc9b12a9b93ad67
Ludde86 <bslbjorn@gmail.com>
2020-04-21 22:41:39 +0200

    fix migrate to react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class App extends Component {
+const App = () => {
 	// @todo - migrate to react hooks
 	// initialize our state
-	state = {
-		data: [],
-		id: 0,
-		message: null,
-		intervalIsSet: false,
-		idToDelete: null,
-		idToUpdate: null,
-		objectToUpdate: null
-	};
+	const [ data, setData ] = useState([]);
+	// const [ id, setId ] = useState(0);
+	const [ message, setMessage ] = useState(null);
+	const [ intervalIsSet, setIntervalIsSet ] = useState(false);
+	const [ idToDelete, setIdToDelete ] = useState(null);
+	const [ idToUpdate, setIdToUpdate ] = useState(null);
+	// const [ updateToApply, setUpdateToApply ] = useState(null);
+	const [ objectToUpdate, setObjectToUpdate ] = useState(null);
 
 	// when component mounts, first thing it does is fetch all existing data in our db
 	// then we incorporate a polling logic so that we can easily see if our db has
 	// changed and implement those changes into our UI
-	componentDidMount() {
-		this.getDataFromDb();
-		if (!this.state.intervalIsSet) {
-			let interval = setInterval(this.getDataFromDb, 1000);
-			this.setState({ intervalIsSet: interval });
-		}
-	}
-
-	// never let a process live forever
-	// always kill a process everytime we are done using it
-	componentWillUnmount() {
-		if (this.state.intervalIsSet) {
-			clearInterval(this.state.intervalIsSet);
-			this.setState({ intervalIsSet: null });
-		}
-	}
+	useEffect(
+		() => {
+			getDataFromDb();
+			if (!intervalIsSet) {
+				let interval = setInterval(getDataFromDb, 1000);
+				setIntervalIsSet(interval);
+			}
+		},
+		[ intervalIsSet ]
+	);
 
 	// just a note, here, in the front end, we use the id key of our data object
 	// in order to identify which we want to Update or delete.
@@ -41,16 +34,14 @@ class App extends Component {
 
 	// our first get method that uses our backend api to
 	// fetch data from our data base
-	getDataFromDb = () => {
-		fetch('http://localhost:3001/api/getData')
-			.then((data) => data.json())
-			.then((res) => this.setState({ data: res.data }));
+	const getDataFromDb = () => {
+		fetch('http://localhost:3001/api/getData').then((data) => data.json()).then((res) => setData(res.data));
 	};
 
 	// our put method that uses our backend api
 	// to create new query into our data base
-	putDataToDB = (message) => {
-		let currentIds = this.state.data.map((data) => data.id);
+	const putDataToDB = (message) => {
+		let currentIds = data.map((data) => data.id);
 		let idToBeAdded = 0;
 		while (currentIds.includes(idToBeAdded)) {
 			++idToBeAdded;
@@ -64,10 +55,11 @@ class App extends Component {
 
 	// our delete method that uses our backend api
 	// to remove existing database information
-	deleteFromDB = (idTodelete) => {
+	const deleteFromDB = (idTodelete) => {
 		parseInt(idTodelete);
 		let objIdToDelete = null;
-		this.state.data.forEach((dat) => {
+		data.forEach((dat) => {
+			// eslint-disable-next-line
 			if (dat.id == idTodelete) {
 				objIdToDelete = dat._id;
 			}
@@ -82,10 +74,11 @@ class App extends Component {
 
 	// our update method that uses our backend api
 	// to overwrite existing data base information
-	updateDB = (idToUpdate, updateToApply) => {
+	const updateDB = (idToUpdate, objectToUpdate) => {
 		let objIdToUpdate = null;
 		parseInt(idToUpdate);
-		this.state.data.forEach((dat) => {
+		data.forEach((dat) => {
+			// eslint-disable-next-line
 			if (dat.id == idToUpdate) {
 				objIdToUpdate = dat._id;
 			}
@@ -93,68 +86,64 @@ class App extends Component {
 
 		axios.post('http://localhost:3001/api/updateData', {
 			id: objIdToUpdate,
-			update: { message: updateToApply }
+			update: { message: objectToUpdate }
 		});
 	};
 
 	// here is our UI
 	// it is easy to understand their functions when you
 	// see them render into our screen
-	render() {
-		const { data } = this.state;
-		return (
-			<div>
-				<ul>
-					{data.length <= 0 ? (
-						'NO DB ENTRIES YET'
-					) : (
-						data.map((dat) => (
-							<li style={{ padding: '10px' }} key={data.message}>
-								<span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
-								<span style={{ color: 'gray' }}> data: </span>
-								{dat.message}
-							</li>
-						))
-					)}
-				</ul>
-				<div style={{ padding: '10px' }}>
-					<input
-						type="text"
-						onChange={(e) => this.setState({ message: e.target.value })}
-						placeholder="add something in the database"
-						style={{ width: '200px' }}
-					/>
-					<button onClick={() => this.putDataToDB(this.state.message)}>ADD</button>
-				</div>
-				<div style={{ padding: '10px' }}>
-					<input
-						type="text"
-						style={{ width: '200px' }}
-						onChange={(e) => this.setState({ idToDelete: e.target.value })}
-						placeholder="put id of item to delete here"
-					/>
-					<button onClick={() => this.deleteFromDB(this.state.idToDelete)}>DELETE</button>
-				</div>
-				<div style={{ padding: '10px' }}>
-					<input
-						type="text"
-						style={{ width: '200px' }}
-						onChange={(e) => this.setState({ idToUpdate: e.target.value })}
-						placeholder="id of item to update here"
-					/>
-					<input
-						type="text"
-						style={{ width: '200px' }}
-						onChange={(e) => this.setState({ updateToApply: e.target.value })}
-						placeholder="put new value of the item here"
-					/>
-					<button onClick={() => this.updateDB(this.state.idToUpdate, this.state.updateToApply)}>
-						UPDATE
-					</button>
-				</div>
+
+	return (
+		<div>
+			<ul>
+				{data.length <= 0 ? (
+					'NO DB ENTRIES YET'
+				) : (
+					data.map((dat) => (
+						<li style={{ padding: '10px' }} key={dat.id}>
+							<span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
+							<span style={{ color: 'gray' }}> data: </span>
+							{dat.message}
+						</li>
+					))
+				)}
+			</ul>
+			<div style={{ padding: '10px' }}>
+				<input
+					type="text"
+					onChange={(e) => setMessage(e.target.value)}
+					placeholder="add something in the database"
+					style={{ width: '200px' }}
+				/>
+				<button onClick={() => putDataToDB(message)}>ADD</button>
 			</div>
-		);
-	}
-}
+			<div style={{ padding: '10px' }}>
+				<input
+					type="text"
+					style={{ width: '200px' }}
+					onChange={(e) => setIdToDelete(e.target.value)}
+					placeholder="put id of item to delete here"
+				/>
+				<button onClick={() => deleteFromDB(idToDelete)}>DELETE</button>
+			</div>
+			<div style={{ padding: '10px' }}>
+				<input
+					type="text"
+					style={{ width: '200px' }}
+					onChange={(e) => setIdToUpdate(e.target.value)}
+					placeholder="id of item to update here"
+				/>
+				<input
+					type="text"
+					style={{ width: '200px' }}
+					onChange={(e) => setObjectToUpdate(e.target.value)}
+					placeholder="put new value of the item here"
+				/>
+				<button onClick={() => updateDB(idToUpdate, objectToUpdate)}>UPDATE</button>
+			</div>
+		</div>
+	);
+};
 
 export default App;
