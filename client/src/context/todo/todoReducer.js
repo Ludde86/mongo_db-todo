9bed2b7174e0913c8a19a7322ea05ace4f8c4bd0
Ludde86 <bslbjorn@gmail.com>
2020-04-23 11:16:26 +0200

    add case GET_DATA

--- client/src/context/todo/todoReducer.js
+++ client/src/context/todo/todoReducer.js
@@ -1,5 +1,13 @@
+import { GET_DATA } from '../types';
+
 export default (state, action) => {
 	switch (action.type) {
+		case GET_DATA:
+			return {
+				...state,
+				todos: action.payload
+				// get all data from
+			};
 		default:
 			return state;
 	}
