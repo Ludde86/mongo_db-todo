be1bcb97900218325dcee490347d9ba5f5ef2067
Ludde86 <bslbjorn@gmail.com>
2020-05-12 10:28:15 +0200

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const express = require('express');
-var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const Data = require('./data');
@@ -8,6 +7,7 @@ const Shopping = require('./shopping');
 
 const API_PORT = 3001;
 const app = express();
+var cors = require('cors');
 app.use(cors());
 const router = express.Router();
 
